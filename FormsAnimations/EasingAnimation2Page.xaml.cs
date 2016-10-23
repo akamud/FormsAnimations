ae336c2870fed520f1db31eb326bc70f3dbc5738
Mahmoud Ali <muddibr@gmail.com>
2016-10-23 19:16:10 -0200

    Complex easing

--- FormsAnimations/EasingAnimation2Page.xaml.cs
+++ FormsAnimations/EasingAnimation2Page.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace FormsAnimations
@@ -35,9 +35,28 @@ namespace FormsAnimations
                 await buttonCustom.ScaleTo(scale, 1000, new Easing(t => (int)(5 * t) / 5.0));
                 await buttonCustom.ScaleTo(1, 1000, (Easing)RandomEase);
             };
+
+            buttonDrop.Clicked += async (sender, e) =>
+            {
+                await FallOverAnimation();
+            };
         }
 
         double RandomEase(double t)
             => t == 0 || t == 1 ? t : t + 0.25 * (random.NextDouble() - 0.5);
+
+        async Task FallOverAnimation()
+        {
+            var navigationHeight = 170;
+
+            buttonDrop.AnchorX = 0;
+            buttonDrop.AnchorY = 1;
+            await buttonDrop.RotateTo(90, 3000, CustomEasing());
+            await buttonDrop.TranslateTo(0, Height - buttonDrop.Height / 2 - buttonDrop.Width - navigationHeight,
+                                           1000, Easing.BounceOut); // BounceOut
+        }
+
+        private Easing CustomEasing()
+            => new Easing(t => 1 - Math.Cos(10 * Math.PI * t) * Math.Exp(-5 * t));
     }
 }
--- FormsAnimations/FormsAnimationsPage.xaml.cs
+++ FormsAnimations/FormsAnimationsPage.xaml.cs
@@ -14,34 +14,7 @@ namespace FormsAnimations
             btnAnchorAnimation.Clicked += async (sender, e) => await Navigation.PushAsync(new AnchorAnimationPage());
             btnEasingAnimation.Clicked += async (sender, e) => await Navigation.PushAsync(new EasingAnimationPage());
             btnEasingAnimation2.Clicked += async (sender, e) => await Navigation.PushAsync(new EasingAnimation2Page());
-            
-            //btnAnimation.Clicked += async (sender, e) =>
-            //{
-            //    //await btnAnimation.RotateTo(90, 250, Easing.SinIn);
-            //    //await btnAnimation.RotateTo(-90, 500, Easing.SinInOut);
-            //    //await btnAnimation.RotateTo(0, 250, Easing.SinOut);
-
-            //    await FallOverAnimation();
-            //};
         }
-
-        //async Task FallOverAnimation()
-        //{
-        //    btnAnimation.AnchorX = 0;
-        //    btnAnimation.AnchorY = 1;
-        //    await btnAnimation.RotateTo(90, 3000,
-        //                                new Easing(t => 1 - Math.Cos(10 * Math.PI * t)
-        //                                           * Math.Exp(-5 * t)));
-        //    await btnAnimation.TranslateTo(0, Height - btnAnimation.Height / 2 - btnAnimation.Width,
-        //                                   1000, Easing.BounceOut);
-
-        //    btnAnimation.AnchorX = 1;
-        //    btnAnimation.AnchorY = 0;
-        //    btnAnimation.TranslationX -= btnAnimation.Width - btnAnimation.Height;
-        //    btnAnimation.TranslationY += btnAnimation.Width - btnAnimation.Height;
-
-        //    await btnAnimation.RotateTo(180, 1000, Easing.BounceOut);
-        //}
     }
 }
 
