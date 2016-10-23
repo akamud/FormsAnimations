141a5a22db2da4ebed47db6167e16521d6ccdc4b
Mahmoud Ali <muddibr@gmail.com>
2016-10-23 15:23:01 -0200

    Initial commit

---
+++ FormsAnimations/BasicAnimationPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FormsAnimations
+{
+    public partial class BasicAnimationPage : ContentPage
+    {
+        public BasicAnimationPage()
+        {
+            InitializeComponent();
+
+            button.Clicked += async (sender, e) =>
+            {
+                await button.RotateTo(360);
+            };
+
+            buttonTempo.Clicked += async (sender, e) =>
+            {
+                //button.Rotation = 0;
+                await buttonTempo.RotateTo(360, 2000);
+            };
+
+            buttonRelative.Clicked += async (sender, e) =>
+            {
+                //buttonRelative.Rotation = 0;
+                await buttonRelative.RelRotateTo(90, 1000);
+            };
+
+            buttonComposto.Clicked += async (sender, e) =>
+            {
+                buttonComposto.Text = "RotateTo 90";
+                await buttonComposto.RotateTo(90, 500);
+                buttonComposto.Text = "RotateTo -90";
+                await buttonComposto.RotateTo(-90, 1000);
+                buttonComposto.Text = "RotateTo 0";
+                await buttonComposto.RotateTo(0, 500);
+            };
+
+            buttonSimultaneo.Clicked += async (sender, e) =>
+            {
+                buttonSimultaneo.Rotation = 0;
+
+                //await Task.WhenAny<bool>(
+                //    buttonSimultaneo.RotateTo(360, 2000),
+                //    buttonSimultaneo.ScaleTo(5, 1000)
+                //);
+
+                await buttonSimultaneo.RotateTo(360, 2000);
+                await buttonSimultaneo.ScaleTo(5, 1000);
+
+                await buttonSimultaneo.ScaleTo(1, 1000);
+            };
+
+            buttonShake.Clicked += async (sender, e) => await Shake(buttonShake);
+
+            //buttonShakeEasing.Clicked += async (sender, e) => await ShakeEasing(buttonShakeEasing);
+        }
+
+        private async Task Shake(VisualElement view)
+        {
+            await view.TranslateTo(5, 0, 25);
+            await view.TranslateTo(-5, 0, 50);
+            await view.TranslateTo(5, 0, 50);
+            await view.TranslateTo(-5, 0, 50);
+            await view.TranslateTo(5, 0, 50);
+            await view.TranslateTo(0, 0, 25);
+        }
+
+        //private async Task ShakeEasing(VisualElement view)
+        //{
+        //    await view.TranslateTo(5, 0, 25, Easing.SinIn);
+        //    await view.TranslateTo(-5, 0, 50, Easing.SinInOut);
+        //    await view.TranslateTo(5, 0, 50, Easing.SinInOut);
+        //    await view.TranslateTo(-5, 0, 50, Easing.SinInOut);
+        //    await view.TranslateTo(5, 0, 50, Easing.SinInOut);
+        //    await view.TranslateTo(0, 0, 25, Easing.SinOut);
+        //}
+    }
+}
---
+++ FormsAnimations/FormsAnimationsPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FormsAnimations
+{
+    public partial class FormsAnimationsPage : ContentPage
+    {
+        Random random = new Random();
+
+        public FormsAnimationsPage()
+        {
+            InitializeComponent();
+
+            btnBasicAnimation.Clicked += async (sender, e) => await Navigation.PushAsync(new BasicAnimationPage());
+
+            //btnAnimation.Clicked += async (sender, e) =>
+            //{
+            //    //await btnAnimation.RotateTo(90, 250, Easing.SinIn);
+            //    //await btnAnimation.RotateTo(-90, 500, Easing.SinInOut);
+            //    //await btnAnimation.RotateTo(0, 250, Easing.SinOut);
+
+            //    //double scale = Math.Min(Width / btnAnimation.Width, Height / btnAnimation.Height);
+            //    //await btnAnimation.ScaleTo(scale, 1000, new Easing(t => (int)(5 * t) / 5.0));
+            //    //await btnAnimation.ScaleTo(1, 1000, (Easing)RandomEase);
+
+            //    await FallOverAnimation();
+            //};
+        }
+
+        //async Task FallOverAnimation()
+        //{
+        //    btnAnimation.AnchorX = 0;
+        //    btnAnimation.AnchorY = 1;
+        //    await btnAnimation.RotateTo(90, 3000,
+        //                                new Easing(t => 1 - Math.Cos(10 * Math.PI * t)
+        //                                           * Math.Exp(-5 * t)));
+        //    await btnAnimation.TranslateTo(0, Height - btnAnimation.Height / 2 - btnAnimation.Width,
+        //                                   1000, Easing.BounceOut);
+
+        //    btnAnimation.AnchorX = 1;
+        //    btnAnimation.AnchorY = 0;
+        //    btnAnimation.TranslationX -= btnAnimation.Width - btnAnimation.Height;
+        //    btnAnimation.TranslationY += btnAnimation.Width - btnAnimation.Height;
+
+        //    await btnAnimation.RotateTo(180, 1000, Easing.BounceOut);
+        //}
+
+        //double RandomEase(double t)
+        //    => t == 0 || t == 1 ? t : t + 0.25 * (random.NextDouble() - 0.5);
+    }
+}
+
